9f4535c451ca4da81b5195306d067a64116e902e
Aashish <aashishdubey.dev@gmail.com>
2025-11-04 23:54:10 +0530

    added user db schema

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -1,1 +1,48 @@
 import { Schema, model, Document } from "mongoose";
+import bcrypt from "bcrypt";
+interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  role: "STUDENT" | "INSTRUCTOR" | "ADMIN";
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 6,
+      select: false,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (
+  password: string
+): Promise<boolean> {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model<IUser>("user", userSchema);
+
+export default User;
